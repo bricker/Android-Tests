3208ac02bdca569a779e9407f8c1d53a0f398a0f
Bryan Ricker <bricker88@gmail.com>
2013-12-30 17:16:35 -0800

    Work on GeoQuiz application

---
+++ GeoQuiz/src/main/java/com/bryanricker/geoquiz/QuizActivity.java
@@ -0,0 +1,15 @@
+package com.bryanricker.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuizActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
